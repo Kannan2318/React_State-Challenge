dd050f40cbc882b8583bb7ad64551e28e5b26335
kannan <kannanraj02318@gmail.com>
2023-09-20 00:27:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div className="container">
+      <h1>State Challenge</h1>
+        <StepSection/>
+        <CountSection/>
+        <ResultSection/>
     </div>
-  );
+  )
 }
 
-export default App;
+function StepSection(){
+  const [step , setStep] = useState(1)  
+  function callStepMinus(){if (step>1)setStep(s =>s - 1)}
+  function callStepPlus(){if (step>=1)setStep(s =>s + 1)}
+  return(
+    <div className="step-section">
+      <button onClick={callStepMinus}>-</button>
+      <span>Step: {step}</span>
+      <button onClick={callStepPlus}>+</button>
+    </div>
+  )
+}
+
+function CountSection(){
+  const [count , setCount] = useState(0)  
+  function callCountMinus(){
+    if(count <= 0 || count >= 0){
+      setCount(c => c - 1)
+    }
+  }
+  function callCountPlus(){
+    if(count <= 0 || count >= 0){
+      setCount(c => c + 1)
+    }
+  }
+  return(
+    <div className="count-section">
+      <button onClick={callCountMinus}>-</button>
+      <span passingCount={count}>Count: {count}</span>
+      <button onClick={callCountPlus}>+</button>
+    </div>
+  )
+}
+function ResultSection({passingCount}){
+  const getYear = new Date().getFullYear();
+  const getDate = new Date().getDate();
+  const getMonth = new Date().getMonth();
+  const getDay = new Date().getDay();
+  const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+  const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec']
+  
+  // console.log(date);
+  return(
+    <div className="result-section">  
+    {console.log({passingCount})}  
+      
+      <p> {passingCount} {days[getDay]} {months[getMonth]} {getDate} {getYear}.</p>
+    </div>
+ 
+  )
+}
